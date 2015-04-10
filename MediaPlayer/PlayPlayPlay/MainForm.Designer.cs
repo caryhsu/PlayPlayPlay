f79c7c8b13144f16d23ff5326e367b47c96e6dea
caryhsu <cary.hsu@gmail.com>
2015-04-10 23:00:04 +0800

    init

---
+++ MediaPlayer/PlayPlayPlay/MainForm.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace PlayPlayPlay
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.portToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.messageToolStripStatusLabel = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripProgressBar1 = new System.Windows.Forms.ToolStripProgressBar();
+            this.pauseTimer = new System.Windows.Forms.Timer(this.components);
+            this.axWindowsMediaPlayer1 = new AxWMPLib.AxWindowsMediaPlayer();
+            this.checkPumpTimer = new System.Windows.Forms.Timer(this.components);
+            this.menuStrip1.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.portToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(456, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(38, 20);
+            this.fileToolStripMenuItem.Text = "&File";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.O)));
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(151, 22);
+            this.openToolStripMenuItem.Text = "&Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // portToolStripMenuItem
+            // 
+            this.portToolStripMenuItem.Name = "portToolStripMenuItem";
+            this.portToolStripMenuItem.Size = new System.Drawing.Size(42, 20);
+            this.portToolStripMenuItem.Text = "&Port";
+            this.portToolStripMenuItem.Click += new System.EventHandler(this.portToolStripMenuItem_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.messageToolStripStatusLabel,
+            this.toolStripProgressBar1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 311);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(456, 22);
+            this.statusStrip1.TabIndex = 2;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // messageToolStripStatusLabel
+            // 
+            this.messageToolStripStatusLabel.Name = "messageToolStripStatusLabel";
+            this.messageToolStripStatusLabel.Size = new System.Drawing.Size(339, 17);
+            this.messageToolStripStatusLabel.Spring = true;
+            this.messageToolStripStatusLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // toolStripProgressBar1
+            // 
+            this.toolStripProgressBar1.Name = "toolStripProgressBar1";
+            this.toolStripProgressBar1.Size = new System.Drawing.Size(100, 16);
+            // 
+            // pauseTimer
+            // 
+            this.pauseTimer.Tick += new System.EventHandler(this.pauseTimer_Tick);
+            // 
+            // axWindowsMediaPlayer1
+            // 
+            this.axWindowsMediaPlayer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.axWindowsMediaPlayer1.Enabled = true;
+            this.axWindowsMediaPlayer1.Location = new System.Drawing.Point(0, 24);
+            this.axWindowsMediaPlayer1.Name = "axWindowsMediaPlayer1";
+            this.axWindowsMediaPlayer1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axWindowsMediaPlayer1.OcxState")));
+            this.axWindowsMediaPlayer1.Size = new System.Drawing.Size(456, 287);
+            this.axWindowsMediaPlayer1.TabIndex = 3;
+            this.axWindowsMediaPlayer1.PlayStateChange += new AxWMPLib._WMPOCXEvents_PlayStateChangeEventHandler(this.axWindowsMediaPlayer1_PlayStateChange);
+            // 
+            // checkPumpTimer
+            // 
+            this.checkPumpTimer.Enabled = true;
+            this.checkPumpTimer.Interval = 10;
+            this.checkPumpTimer.Tick += new System.EventHandler(this.checkPumpTimer_Tick);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(456, 333);
+            this.Controls.Add(this.axWindowsMediaPlayer1);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "MainForm";
+            this.Text = "Play! Play! Play!";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel messageToolStripStatusLabel;
+        private System.Windows.Forms.ToolStripProgressBar toolStripProgressBar1;
+        private System.Windows.Forms.Timer pauseTimer;
+        private AxWMPLib.AxWindowsMediaPlayer axWindowsMediaPlayer1;
+        private System.Windows.Forms.Timer checkPumpTimer;
+        private System.Windows.Forms.ToolStripMenuItem portToolStripMenuItem;
+    }
+}
+
---
+++ MediaPlayer/PlayPlayPlay/MainForm.cs
@@ -0,0 +1,266 @@
+﻿using System;
+using System.Management;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.IO.Ports;
+
+namespace PlayPlayPlay
+{
+    public partial class MainForm : Form
+    {
+
+        public int PAUSE_AFTER_MSEC = 200;
+
+        private List<String> serialPorts = new List<String>();
+        private SerialPort mySerialPort;
+
+        public MainForm()
+        {
+            InitializeComponent();
+            InitialSerialPorts();
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog open = new OpenFileDialog();
+            open.Title = "Open";
+            open.Filter = "All Files|*.*";
+            try
+            {
+                if (open.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                {
+                    axWindowsMediaPlayer1.URL = open.FileName;
+                    messageToolStripStatusLabel.Text = open.FileName;
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void pauseToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            WMPLib.IWMPControls3 controls = (WMPLib.IWMPControls3)axWindowsMediaPlayer1.Ctlcontrols;
+            controls.pause();
+        }
+
+        private void InitialSerialPorts()
+        {
+            List<String> mySerialPorts = new List<String>();
+            using (var searcher = new ManagementObjectSearcher("SELECT * FROM WIN32_SerialPort"))
+            {
+                string[] portnames = SerialPort.GetPortNames();
+                var ports = searcher.Get().Cast<ManagementBaseObject>().ToList();
+                var tList = (from n in portnames
+                             join p in ports on n equals p["DeviceID"].ToString()
+                             select n + " - " + p["Caption"]).ToList();
+                foreach (string s in tList)
+                {
+                    mySerialPorts.Add(s);
+                }
+            }
+            if (!ListEquals(this.serialPorts, mySerialPorts))
+            {
+                InitialSerialPortsMenus(mySerialPorts);
+                this.serialPorts = mySerialPorts;
+            }
+
+        }
+
+        private bool ListEquals(List<string> list1, List<string> list2)
+        {
+            if (list1.Count != list2.Count)
+            {
+                return false;
+            }
+            for (int i = 0; i < list1.Count; i++ )
+            {
+                string text1 = list1[i];
+                string text2 = list2[i];
+                if (!text1.Equals(text2)) return false;
+            }
+            return true;
+        }
+
+        private void InitialSerialPortsMenus(List<String> serialPorts)
+        {
+            portToolStripMenuItem.DropDownItems.Clear();
+            foreach (string s in serialPorts)
+            {
+                ToolStripMenuItem portMenu = new ToolStripMenuItem(s);
+                portMenu.CheckOnClick = true;
+                portMenu.Click += new EventHandler(portMenu_Click);
+                portToolStripMenuItem.DropDownItems.Add(portMenu);
+            }
+        }
+
+
+        private void portMenu_Click(object sender, EventArgs e)
+        {
+            // Set the current clicked item to item
+            ToolStripMenuItem item = sender as ToolStripMenuItem;
+            // Loop through all items in the subMenu and uncheck them but do check the clicked item
+            foreach (ToolStripMenuItem tempItemp in ((ToolStripMenuItem)item.OwnerItem).DropDownItems)
+            {
+                if (tempItemp == item)
+                    tempItemp.Checked = true;
+                else
+                    tempItemp.Checked = false;
+            }
+
+            Console.WriteLine("menu.Text=" + item.Text);
+            initMySerialPort(item);
+        }
+
+        private void initialMySerialPort1()
+        {
+            foreach (ToolStripMenuItem tempItemp in portToolStripMenuItem.DropDownItems)
+            {
+                if (tempItemp.Checked)
+                {
+                    initMySerialPort(tempItemp);
+                }
+            }
+        }
+
+        private void initMySerialPort(ToolStripMenuItem item)
+        {
+            this.mySerialPort = new SerialPort("COM4");
+            this.mySerialPort.BaudRate = 9600;
+            this.mySerialPort.Parity = Parity.None;
+            this.mySerialPort.StopBits = StopBits.One;
+            this.mySerialPort.DataBits = 8;
+            this.mySerialPort.Handshake = Handshake.None;
+            this.mySerialPort.ReadBufferSize = 1024000;
+
+            try
+            {
+                this.mySerialPort.Open();
+            }
+            catch (Exception ex)
+            {
+                item.Checked = false;
+                this.mySerialPort = null;
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void axWindowsMediaPlayer1_PlayStateChange(object sender, AxWMPLib._WMPOCXEvents_PlayStateChangeEvent e)
+        {
+            if (e.newState == 2) // Paused
+            {
+                //checkPumpTimer.Enabled = true;
+                pauseTimer.Enabled = false;
+            }
+            else if (e.newState == 3) // Playing
+            {
+                //checkPumpTimer.Enabled = true;
+                pauseAfter(PAUSE_AFTER_MSEC);
+            }
+            else
+            {
+                //checkPumpTimer.Enabled = true;
+            }
+        }
+
+        private void pauseAfter(int interval)
+        {
+            pauseTimer.Enabled = false;
+            pauseTimer.Interval = interval;
+            pauseTimer.Enabled = true;
+        }
+
+        private void pauseTimer_Tick(object sender, EventArgs e)
+        {
+            pauseTimer.Enabled = false;
+            pause();
+        }
+
+        private void pause()
+        {
+            WMPLib.IWMPControls3 controls = (WMPLib.IWMPControls3)axWindowsMediaPlayer1.Ctlcontrols;
+            if (axWindowsMediaPlayer1.playState == WMPLib.WMPPlayState.wmppsPlaying) // Playing
+            {
+                controls.pause();
+            }
+        }
+
+        private void play()
+        {
+            WMPLib.IWMPControls3 controls = (WMPLib.IWMPControls3)axWindowsMediaPlayer1.Ctlcontrols;
+            if (axWindowsMediaPlayer1.playState == WMPLib.WMPPlayState.wmppsPaused) // Paused
+            {
+                controls.play();
+            }
+        }
+
+        private void pump()
+        {
+            play();
+            pauseAfter(PAUSE_AFTER_MSEC);
+        }
+
+        private void checkPumpTimer_Tick(object sender, EventArgs e)
+        {
+            //InitialSerialPorts();
+            //initialMySerialPort1();
+            bool isPump = checkPump();
+            if (isPump)
+            {
+                pump();
+            }
+        }
+
+        private void serialPortDataReceivedHandler(object sender, SerialDataReceivedEventArgs e)
+        {
+            SerialPort sp = (SerialPort)sender;
+            string indata = sp.ReadExisting();
+            pump();
+            Console.WriteLine("Data Received:" + indata);
+        }
+
+        private bool checkPump()
+        {
+            if (this.mySerialPort == null)
+            {
+                return false;
+            }
+            string indata = null;
+            try
+            {
+                indata = this.mySerialPort.ReadExisting();
+            }
+            catch (Exception ex)
+            {
+                /**
+                if (this.mySerialPort != null)
+                {
+                    try
+                    {
+                        this.mySerialPort.Open();
+                    }
+                    catch (Exception e)
+                    {
+                    }
+                }
+                 */
+            }
+            return indata == null || indata.Length > 0;
+        }
+
+        private void portToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.InitialSerialPorts();
+        }
+
+    }
+
+}
